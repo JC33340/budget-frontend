5ccd7f45bb78b3c5895083c055e79a54cc490b0b
Jason Chan <jasonchanhw0531@gmail.com>
2025-03-20 21:59:39 +0000

    added calling db pinging functions and loading screen

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { validateToken } from '../utils/auth.utils';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 const AuthLayout = () => {
     const navigate = useNavigate();
+
+    const [dbActive, setDbActive] = useState(false);
 
     useEffect(() => {
         const jwt = localStorage.getItem('budgeter_jwt');
@@ -18,22 +21,66 @@ const AuthLayout = () => {
             }
         };
 
+        const wakedb = async () => {
+            const result = await fetch(`${import.meta.env.VITE_API}/pingdb`);
+            if (!result.ok) {
+                return alert('Sorry something went wrong');
+            }
+            setDbActive(true);
+        };
+
+        wakedb();
         checkToken();
     }, []);
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-8 h-full w-full border-2 border-light-blue rounded-lg overflow-hidden">
+        <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-y-8 h-full w-full border-2 border-light-blue rounded-lg overflow-hidden">
+            <div className="absolute top-4 left-4 border-2 bg-white p-2 rounded-lg">
+                <p className="text-blue">Demo account :</p>
+                <div className="text-light-gray">
+                    <p>Login : demoaccount@demo.com</p>
+                    <p>Password : demoaccount123</p>
+                </div>
+            </div>
             <div className="flex items-end lg:items-center justify-center">
                 <div className="flex flex-col lg:items-start items-center gap-y-4">
-                    <p className="text-5xl sm:text-7xl font-bold text-blue animate-enter-right">Budgeter</p>
-                    <p className=" text-center lg:text-start text-light-gray animate-enter-right" style={{animationDelay:'0.1s', animationFillMode:'both',animationDirection:'normal'}}>
+                    <p className="text-5xl sm:text-7xl font-bold text-blue animate-enter-right">
+                        Budgeter
+                    </p>
+                    <p
+                        className=" text-center lg:text-start text-light-gray animate-enter-right"
+                        style={{
+                            animationDelay: '0.1s',
+                            animationFillMode: 'both',
+                            animationDirection: 'normal',
+                        }}
+                    >
                         An app built using ReactJS, ExpressJS and SQL
                     </p>
                 </div>
             </div>
-            <div className="flex items-start lg:items-center justify-center animate-enter-left"  style={{animationDelay:'0.2s', animationFillMode:'both',animationDirection:'normal'}}>
+            <div
+                className="flex items-start lg:items-center justify-center animate-enter-left"
+                style={{
+                    animationDelay: '0.2s',
+                    animationFillMode: 'both',
+                    animationDirection: 'normal',
+                }}
+            >
                 <div className="flex flex-col gap-y-4 items-center border-2 border-light-blue rounded-lg p-6 w-70 sm:w-90">
-                    <Outlet />
+                    {dbActive ? (
+                        <Outlet />
+                    ) : (
+                        <div className="flex flex-col gap-y-4 items-center">
+                            <p className="font-semibold text-dark-gray">
+                                Loading
+                            </p>
+                            <AiOutlineLoading3Quarters
+                                className="animate-spin fill-dark-gray"
+                                size={50}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
